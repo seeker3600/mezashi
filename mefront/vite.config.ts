0d423d26b35113e743c3b014aecb4a8456fabfc2
seeker3600 <29515315+seeker3600@users.noreply.github.com>
2026-07-20 20:28:21 +0900

    デプロイを修正。

--- mefront/vite.config.ts
+++ mefront/vite.config.ts
@@ -5,6 +5,7 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/mezashi/",
 	plugins: [
 		react(),
 		tailwindcss(),
